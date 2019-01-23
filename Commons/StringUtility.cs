fe8dc9b490d092b32289ba70214f6d4e0dbee785
hodaka <hbando@microcircus.com>
2019-01-23 11:31:28 +0900

    空のプロジェクトを追加

---
+++ Commons/StringUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons
+{
+    /// <summary>
+    ///     文字列に関するユーティリティ
+    /// </summary>
+    public class StringUtility
+    {
+
+    }
+}
